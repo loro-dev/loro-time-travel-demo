9a9f751133a08b63abf08f6e1ae441a3f3a87329
leeeon233 <leeeon233@gmail.com>
2023-11-12 21:04:26 +0800

    feat: history

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,107 @@
-import { useEffect, useRef, useState } from "react";
-import { Slider, Text, Theme } from "@radix-ui/themes";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Slider, Theme } from "@radix-ui/themes";
 // @ts-ignore
 import encodedBase64 from "/seph-blog1.txt?url&raw";
 import { Loro, OpId, setPanicHook } from "loro-crdt";
 import "./App.css";
 import "@radix-ui/themes/styles.css";
+import { throttle } from "throttle-debounce";
 import { base64ToBinary } from "./utils";
 
+setPanicHook();
 function App() {
-  const [text, setText] = useState("");
+  const [checkoutTime, setCheckoutTime] = useState(0);
+  const text = useRef("");
+  const view = useRef(null);
   const [version, setVersion] = useState(0);
   const loro = useRef(new Loro());
   const lastId = useRef<undefined | OpId>();
 
   useEffect(() => {
-    setPanicHook();
     const bytes = base64ToBinary(encodedBase64);
     loro.current.import(bytes);
-    loro.current.subscribe((event) => {
-      console.log(event);
-    });
-    for (let [_, changes] of loro.current.getAllChanges().entries()) {
-      console.log(changes);
-      const lastChange = changes[changes.length - 1];
-      lastId.current = {
-        peer:  lastChange.peer.valueOf(),
-        counter: lastChange.counter + lastChange.length - 1,
-      };
-    }
-    const text = loro.current.getText("text").toString();
+    lastId.current = loro.current.frontiers()[0];
+    lastId.current.counter -= 10;
+    const textString = loro.current.getText("text").toString();
     setVersion(lastId.current!.counter);
-    setText(text);
+    text.current = textString;
   }, []);
 
-  const checkout = (version: number)=>{
-    console.log(loro.current.getAllChanges());
-    console.log(loro.current.frontiers());
-    loro.current.checkout([{
-      peer: lastId.current?.peer ?? 0n,
-      counter: version
-    }])
-  }
-
   return (
-    <>
+    <div style={{ width: "100%" }} ref={view}>
       <Theme>
         <Slider
           value={[version]}
           max={lastId.current?.counter ?? 0}
-          onValueChange={(v) => {
+          min={-1}
+          onValueChange={useCallback((v: number[]) => {
+            const checkout = throttle(100, (version: number) => {
+              const start = performance.now();
+              if (version === -1) {
+                loro.current.checkout([]);
+              } else {
+                loro.current.checkout([{
+                  peer: lastId.current?.peer ?? 0n,
+                  counter: version,
+                }]);
+              }
+              setCheckoutTime(performance.now() - start);
+              text.current = loro.current.getText("text").toString();
+            });
+            console.log(v[0]);
             setVersion(v[0]);
             checkout(v[0]);
-          }}
+          }, [])}
         />
-        <Text>{text}</Text>
+        <div style={{ display: "flex" }}>
+          <span style={{ width: "200px" }}>Current Version {version}</span>{" "}
+          <span style={{ width: "200px" }}>Max Version {lastId.current?.counter ?? 0}</span>
+          <span>Checkout duration: {checkoutTime.toFixed(2)} ms</span>
+        </div>
+        <div>Current Document Text length: {text.current.length}</div>
+        <div style={{ position: "relative" }}>
+          <div
+            style={{
+              width: "100%",
+              whiteSpace: "pre-wrap",
+              transform: "scale(0.8)",
+              transformOrigin: "0 0",
+              position: "absolute",
+              top: 0,
+              left: 0,
+            }}
+          >
+            {text.current}
+          </div>
+          <div
+            style={{
+              width: "100%",
+              whiteSpace: "pre-wrap",
+              transform: "scale(0.1) translateX(800%)",
+              transformOrigin: "0 0",
+              position: "absolute",
+              top: 0,
+              left: 0,
+            }}
+          >
+            {text.current}
+          </div>
+          <div
+            style={{
+              width: "100%",
+              whiteSpace: "pre-wrap",
+              transform: "scale(0.02) translateX(4500%)",
+              transformOrigin: "0 0",
+              position: "absolute",
+              top: 0,
+              left: 0,
+            }}
+          >
+            {text.current}
+          </div>
+        </div>
       </Theme>
-    </>
+    </div>
   );
 }
 
