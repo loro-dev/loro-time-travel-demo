7b0d2fb6260259b684f63987091ea733043cb09f
Zixuan Chen <remch183@outlook.com>
2024-10-09 09:56:08 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),wasm(), topLevelAwait()],
-})
+  base: "/loro-time-travel-demo/",
+  plugins: [react(), wasm(), topLevelAwait()],
+});
